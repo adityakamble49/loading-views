f17e68b2e63b40966199e2dcc8838268ff6ef5dd
Aditya Kamble <adityakamble49@gmail.com>
2018-08-02 17:06:36 +0530

    Extension Utils added

---
+++ loadingviews/src/main/java/com/adityakamble49/loadingviews/utils/Extensions.kt
@@ -0,0 +1,32 @@
+package com.adityakamble49.loadingviews.utils
+
+/**
+ * Extensions
+ *
+ * @author Aditya Kamble
+ * @since 2/8/2018
+ */
+
+/**
+ * Convert Integer To Float Value
+ */
+val Int.f
+    get() = this.toFloat()
+
+/**
+ * Convert Float To Int Value
+ */
+val Float.i
+    get() = this.toInt()
+
+/**
+ * Convert Double To Int Value
+ */
+val Double.i
+    get() = this.toInt()
+
+/**
+ * Convert Double To Float Value
+ */
+val Double.f
+    get() = this.toFloat()
